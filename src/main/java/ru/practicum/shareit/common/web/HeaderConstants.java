49c2e7307c3a3897346a684fdddaf46ab225741e
madoka <obivanyakenobi@gmail.com>
2025-12-26 13:17:53 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/common/web/HeaderConstants.java
+++ src/main/java/ru/practicum/shareit/common/web/HeaderConstants.java
@@ -2,7 +2,8 @@ package ru.practicum.shareit.common.web;
 
 public final class HeaderConstants {
 
-    private HeaderConstants() {}
+    private HeaderConstants() {
+    }
 
     public static final String USER_ID = "X-Sharer-User-Id";
 }
