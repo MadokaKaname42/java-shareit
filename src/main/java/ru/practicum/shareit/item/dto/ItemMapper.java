567eaf3c15bc6b5af91e68b97aa1b43c96982594
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-01-06 01:40:33 +0300

    Update ItemMapper.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.model.Item;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
@@ -9,4 +10,6 @@ public interface ItemMapper {
     Item itemDtoToItemModel(ItemDto itemDto);
 
     ItemDto itemModelToItemDto(Item item);
+
+    List<ItemDto> mapItemsToDtos(List<Item> item);
 }
