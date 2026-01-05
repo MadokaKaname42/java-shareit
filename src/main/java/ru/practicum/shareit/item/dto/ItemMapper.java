d6975feb4f8f14e549e3da05719abd509f915719
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-01-06 01:31:41 +0300

    Update ItemMapper.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -1,11 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
-import org.mapstruct.Context;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import ru.practicum.shareit.comments.dto.CommentDto;
-import ru.practicum.shareit.comments.model.Comment;
-import ru.practicum.shareit.comments.repository.CommentRepository;
 import ru.practicum.shareit.item.model.Item;
 
 @Mapper(componentModel = "spring")
