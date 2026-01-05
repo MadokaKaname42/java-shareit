c5836cef11ba937ad42d497a5d16a1a87b8ffcea
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-01-06 01:19:07 +0300

    Update ItemMapper.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -8,27 +8,10 @@ import ru.practicum.shareit.comments.model.Comment;
 import ru.practicum.shareit.comments.repository.CommentRepository;
 import ru.practicum.shareit.item.model.Item;
 
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
-
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
 
     Item itemDtoToItemModel(ItemDto itemDto);
 
     ItemDto itemModelToItemDto(Item item);
-
-
-    @Mapping(target = "comments", expression = "java(getComments(item.getId(), commentRepository))")
-    ItemDto itemModelToItemDto(Item item, @Context CommentRepository commentRepository);
-
-    default List<CommentDto> getComments(Long itemId, CommentRepository commentRepository) {
-        return commentRepository.findAllByItemId(itemId)
-                .stream()
-                .map(this::commentModelToCommentDto)
-                .collect(toList());
-    }
-
-    CommentDto commentModelToCommentDto(Comment comment);
 }
