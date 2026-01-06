8341b89291fd8aa1766fae6d2b8443ee0fdde63a
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-01-06 05:05:38 +0300

    Update ItemRepository.java

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerId(Long ownerId);
 
-    @Query("SELECT i FROM Item i WHERE " +
+    @Query("SELECT i FROM items i WHERE " +
        "(LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
        "LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-       "AND i.available = true")
+       "AND i.is_available = true")
     List<Item> findBySearchText(@Param("text") String text);
     }
 }
