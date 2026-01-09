57dafe29a632c2300173829bcdc09848a89f5a23
madoka <obivanyakenobi@gmail.com>
2026-01-09 12:05:11 +0300

    разделение проекта на микросервисы gateway и server

--- src/main/java/ru/practicum/shareit/common/web/HeaderConstants.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.common.web;
-
-public final class HeaderConstants {
-
-    private HeaderConstants() {
-    }
-
-    public static final String USER_ID = "X-Sharer-User-Id";
-}
